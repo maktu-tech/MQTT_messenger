7b94b6a66c642e1355494c02ebf124a82d62c573
maktu-tech <mayankbadjatiya4747@gmail.com>
2021-05-02 23:07:50 +0530

    second commit

--- model.py
+++ model.py
@@ -1,63 +1,214 @@
+import paho.mqtt.client as mqtt
+import threading
+import time
 from tkinter import *
-def add_emoji(m):
-    m1=msg_entry.get()
-    msg_entry.delete(0,'end')
-    msg_entry.insert(0,m1+m)
+import sys
+
+def on_connect(client, userdata, flags, rc):
+    if rc != 0:
+        print("Connect returned result code: " + str(rc))
     return
-def send():
+def subFunc(client, userdata, msg):
+    global cur_tpub
+    msg_dict[msg.topic]+=msg.payload.decode("utf-8")
+    if(cur_tpub==msg.topic[:-3]):
+        msg_label.configure(text=msg_dict[msg.topic])
+    return
+def sub_imp():
+    client.message_callback_add("auth_ser@"+name, authrec)
+    client.message_callback_add("serv@"+name, eventNews)
+    client.subscribe("auth_ser@"+name)
+    client.subscribe("serv@"+name)
+    return
+def check(n1, p1):
+    global name, fk, pws
+    name = n1
+    root.title('User: '+name)
+    fk = open(n1+".ps","a+")
+    fk.seek(0,0)
+    if(fk.readlines() == []):
+        pws=p1
+        fk.write("name: "+n1+'\n')//6
+        fk.write("pass: "+p1+'\n')
+        info(" Welcome, SignUp Success..\n")
+        sub_imp()
+        frame0.pack_forget()
+    else:
+        fk.seek(0,0)
+        data = fk.readlines()
+        pws_ret=data[1][6:-1]
+        pws=pws_ret
+        if(p1==pws_ret):
+            if(len(data)>2):
+                info(" Welcome, Login Success..\n")
+                sub_imp()
+                if(len(data[2])<8):
+                    frame0.pack_forget()
+                    return()        
+                tsub.extend(data[2][6:-2].split(","))
+                tpub.extend(data[3][6:-2].split(","))
+                
+            for i in range(len(tsub)):
+                client.message_callback_add(tsub[i], subFunc)
+                client.subscribe(tsub[i])
+                tsub_btn.append(Button(frame3, text=tpub[i], command=lambda c=i: topD(c)))
+                tsub_btn[i].place(x=5,y=5+50*i,width=40, height=40)
+                msg_dict[tsub[i]]=""
+            frame0.pack_forget()
+        else:
+            label_msg.configure(text=" Wrong Password, Try again")
+    return
+
+def topD(index):
+    global cur_tpub
+    frame2.pack(side=BOTTOM, fill=X)
+    cur_tpub=tpub[index]
+    msg_label.configure(text=msg_dict[tsub[index]])
+    return
+def info(m1=""):
+    global cur_tpub
+    cur_tpub=""
+    frame2.pack_forget()
+    global msg_info
+    if(m1!=""):
+        t1=time.localtime()
+        msg_info+=str(time.strftime("%H:%M:%S",t1))+" :"+m1+"\n"
+    msg_label.configure(text=msg_info)
+    return
+
+def eventNews(client, userdata, msg):
+    info("<SerEv> "+msg.payload.decode("utf-8"))
+    return
+def authsend(tpub,pasw):
+    client.publish("auth_to_ser",name+','+tpub+','+pasw)
+    frame0.pack_forget()
+    frame3.pack(side=RIGHT, fill=X)
+    frame1.pack(side=TOP, fill=X)
+    frame2.pack(side=BOTTOM, fill=X)
+        
+    return
+def authrec(client, userdata, msg):
+    #auth_ser@
+    #tpub,tsub
+    auth_ret=msg.payload.decode("utf-8").split(",")
+    if(len(auth_ret)>0):
+        tpub.append(auth_ret[0])
+        tsub.append(auth_ret[1])
+        i=(len(tsub)-1)
+        client.message_callback_add(tsub[-1], subFunc)
+        client.subscribe(tsub[-1])
+        tsub_btn.append(Button(frame3, text=tpub[-1], command=lambda c=i: topD(c)))
+        tsub_btn[-1].place(x=5,y=5+50*i,width=40, height=40)
+        msg_dict[tsub[i]]=""
+    return
+def pubFunc():
     global msg_string
+    global cur_tpub
     if(msg_entry.get()==""):
         return
-    msg_string+="\n <me> "
-    msg_string+=msg_entry.get()
+    client.publish(cur_tpub,name+"<@>"+ msg_entry.get()+"\n")
     msg_entry.delete(0,'end')
-    msg_label.configure(text=msg_string)
     return
-msg_string=""
+def addT():
+    frame1.pack_forget()
+    frame2.pack_forget()
+    frame3.pack_forget()
+    
+    label_msg.configure(text="Enter Topic Cred.")
+    #configure topic
+    button_ok.configure(command=lambda: authsend(text_name.get(), text_pass.get()))
+    frame0.pack()
+    return
+
+def close_butt():
+    if(fk==None):
+        root.destroy()
+        sys.exit()
+    fk.truncate(0)
+    #print("tsub",tsub)
+    fk.write("name: "+name+"\n")
+    fk.write("pass: "+pws+'\n')
+    tmp=""
+    for i in tsub:
+        tmp+=i+","
+    fk.write("tsub: "+tmp+'\n')
+    tmp=""
+    for i in tpub:
+        tmp+=i+","
+    fk.write("tpub: "+tmp+'\n')
+    fk.close()
+    root.destroy()
+    sys.exit()
+
+# -- .ps file ---
+topic_names = []
+cur_tpub=""
+pws=""
+tsub=[]
+tpub=[]
+tsub_btn=[]
+msg_dict={}
+msg_info=""
+name =""
+fk=None
+#---frame 0--
 root =Tk()
-root.geometry('304x500')
-root.title('lolChat')
+root.geometry('354x500')
+root.title('lolChat>>>')
+root.protocol("WM_DELETE_WINDOW", close_butt)
+frame0=Frame(root, width=354, height=500, background='#ededed')
+frame0.pack(side=TOP, fill=X)
+label_msg=Label(frame0,text="Hello,\n Enter Your Credentials", font=("Times Roman",12),anchor=CENTER)
+label_msg.place(x=105, y=120)
+label_name=Label(frame0, text="Name: ",  font=("Times Roman",12))
+label_name.place(x=55, y=180)
+text_name=Entry(frame0, justify=LEFT, font=("", 12))
+text_name.place(x=125, y=180, width=150)
+label_pass=Label(frame0, text="Pass: ",  font=("Times Roman",12))
+label_pass.place(x=55, y=230)
+text_pass=Entry(frame0, justify=LEFT, font=("", 12))
+text_pass.place(x=125, y=230, width=150)
+button_ok=Button(frame0, text="Ok", command=lambda: check(text_name.get(), text_pass.get()) ,font=("", 12))
+button_ok.place(x=150, y=270,width=50)
+
+
+
+# client creation
+client = mqtt.Client()
+client.on_connect = on_connect
+client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
+client.username_pw_set("example", "Example123")
+client.connect("49d1a22e3af240efb207d443419e3257.s1.eu.hivemq.cloud", 8883)
+
+   
+t3=threading.Thread(target=client.loop_forever)
+t3.daemon = True
+t3.start()
+
+
 frame1=Frame(root, width=304, height=420, background="#ededed")
-frame1_5=Frame(root, width= 304, height=29,background="#ededed")
+#frame1_5=Frame(root, width= 304, height=29,background="#ededed")
 frame2=Frame(root, width=304, height=50, background="#d4ffd4")
-
-
+frame3=Frame(root, width=50, height=500,background="#d1dfea")
+button_serv=Button(frame3, text="info", command=info)
+button_addt=Button(frame3, text="+", command= addT)
+button_serv.place(x=5, y=405, width=40, height=40)
+button_addt.place(x=5, y=455, width=40, height=40)
+frame3.pack(side=RIGHT, fill=X)
 frame1.pack(side=TOP, fill=X)
-msg_label=Label(frame1,text=msg_string, justify=LEFT,font=("Times Roman",12),anchor=W)
+msg_label=Label(frame1,text=msg_info, justify=LEFT,font=("Times Roman",12),anchor=W)
 msg_label.place(x=5, y=5,width=294, height = 300 )
 
 
 
 frame2.pack(side=BOTTOM, fill=X)
 msg_entry = Entry(frame2,justify=LEFT, font=("", 14))
-msg_button = Button(frame2, text=">>>",command=send)
+msg_button = Button(frame2, text=">>>",command=pubFunc)
 msg_entry.place(x=5, y= 5, width=240, height = 40)
 msg_button.place(x=254, y= 5, width=40, height = 40)
 
-frame1_5.pack(side=BOTTOM, fill=X)
-e_1 = Button(frame1_5, text="\U0001F606" ,font=("",12), command=lambda: add_emoji("\U0001F606"))
-e_1.place(x=2, y= 2, width=25, height = 25)
-e_2 = Button(frame1_5, text="\U0001F923" ,font=("",12), command=lambda: add_emoji("\U0001F923"))
-e_2.place(x=27, y= 2, width=25, height = 25)
-e_3 = Button(frame1_5, text="\U0001F643" ,font=("",12), command=lambda: add_emoji("\U0001F643"))
-e_3.place(x=52, y= 2, width=25, height = 25)
-e_4 = Button(frame1_5, text="\U0001F605" ,font=("",12), command=lambda: add_emoji("\U0001F605"))
-e_4.place(x=77, y= 2, width=25, height = 25)
-e_5 = Button(frame1_5, text="\U0001F607" ,font=("",12), command=lambda: add_emoji("\U0001F607"))
-e_5.place(x=102, y= 2, width=25, height = 25)
-e_6 = Button(frame1_5, text="\U0001F611" ,font=("",12), command=lambda: add_emoji("\U0001F611"))
-e_6.place(x=127, y= 2, width=25, height = 25)
-e_7 = Button(frame1_5, text="\U0001F60F" ,font=("",12), command=lambda: add_emoji("\U0001F60F"))
-e_7.place(x=152, y= 2, width=25, height = 25)
-e_8 = Button(frame1_5, text="\U0001F612" ,font=("",12), command=lambda: add_emoji("\U0001F612"))
-e_8.place(x=177, y= 2, width=25, height = 25)
-e_9 = Button(frame1_5, text="\U0001F44C" ,font=("",12), command=lambda: add_emoji("\U0001F44C"))
-e_9.place(x=202, y= 2, width=25, height = 25)
-e_10 = Button(frame1_5, text="\U0001F918" ,font=("",12), command=lambda: add_emoji("\U0001F918"))
-e_10.place(x=227, y= 2, width=25, height = 25)
-e_11 = Button(frame1_5, text="\U0001F44D" ,font=("",12), command=lambda: add_emoji("\U0001F44D"))
-e_11.place(x=252, y= 2, width=25, height = 25)
-e_12 = Button(frame1_5, text="\U0001F64F" ,font=("",12), command=lambda: add_emoji("\U0001F64F"))
-e_12.place(x=277, y= 2, width=25, height = 25)
 
-root.mainloop()
+t2=threading.Thread(target=root.mainloop())
+t2.daemon = True
+t2.start()
+
--- msg_2win.py
+++ msg_2win.py
@@ -1,117 +1,167 @@
-from collections import namedtuple
 import paho.mqtt.client as mqtt
 import threading
 from tkinter import *
-from tkinter import simpledialog
+
 def on_connect(client, userdata, flags, rc):
     if rc != 0:
         print("Connect returned result code: " + str(rc))
     return
-def lis(client, userdata, msg):
-    global msg_string
-    msg_string+=msg.payload.decode("utf-8")
-    msg_label.configure(text=msg_string)
-
-def add_emoji(m):
-    m1=msg_entry.get()
-    msg_entry.delete(0,'end')
-    msg_entry.insert(0,m1+m)
+def eventNews(client, userdata, msg):
+    print("<Event> "+msg.payload.decode("utf-8"))
     return
-def send():
-    global msg_string
-    global topic_name
-    if(msg_entry.get()==""):
+def authsend(tpub,pasw):
+    client.publish("auth_to_ser",name+','+tpub+','+pasw)
+    print("ok till")
+    return
+def authrec(client, userdata, msg):
+    #tpub,tsub
+    auth_ret=msg.payload.decode("utf-8").split(",")
+    if(len(auth_ret)>0):
+        tpub.append(auth_ret[0])
+        tsub.append(auth_ret[1])
+    return
+def subFunc(client, userdata, msg):
+    print("how")
+    print(msg.payload.decode("utf-8"))
+    return
+def pubFunc(msgs,pub_topic):
+    client.publish(pub_topic,msgs)
+    return
+# def lis(client, userdata, msg):
+#     global msg_string
+#     msg_string+=msg.payload.decode("utf-8")
+#     msg_label.configure(text=msg_string)
+
+# def add_emoji(m):
+#     m1=msg_entry.get()
+#     msg_entry.delete(0,'end')
+#     msg_entry.insert(0,m1+m)
+#     return
+# def send():
+#     global msg_string
+#     global topic_name
+#     if(msg_entry.get()==""):
+#         return
+#     client.publish(topic_name,"\n <"+ name+"> " + msg_entry.get())
+#     msg_entry.delete(0,'end')
+#     msg_label.configure(text=msg_string)
+#     return
+# def subscribe_all():
+#     client.subscribe(topic_name)
+#    return
+def add_topic():
+    flg = input("Do you want to enter topic(Y/N): ")
+    if(flg == 'Y'):
+        t_p_name = input("Enter Topic Name: ")
+        t_p_pass = input("Enter Topic Password: ")
+        authsend(t_p_name, t_p_pass)
+        add_topic()
+    elif(flg == 'N'):
+        print(msg_rep)
         return
-    client.publish(topic_name,"\n <"+ name+"> " + msg_entry.get())
-    msg_entry.delete(0,'end')
-    msg_label.configure(text=msg_string)
-    return
-def extract():
-    global topic_name, name
-    topic_name=msg_en_top.get()
-    if(msg_en_nam.get()==""):
-        name="anonymous"
     else:
-        name=msg_en_nam.get()
-    root1.destroy()
+        add_topic()
 
-topic_name, name = "", ""
-root1 = Tk()
-root1.geometry('400x180')
-root1.title('Join Win')
-frame1=Frame(root1, width= 400, height=180, background='#ededed')
-frame1.pack(side=TOP, fill=X)
-msg_lab_top = Label(frame1, text="Enter Topic: ", font=("", 11))
-msg_en_top = Entry(frame1,justify=LEFT, font=("", 11))
-msg_lab_nam = Label(frame1, text="Enter UserID: ", font=("", 11))
-msg_en_nam = Entry(frame1,justify=LEFT, font=("", 11))
-msg_button = Button(frame1, text="Ok",command=extract)
+# -- .ps file ---
+topic_names = []
+pws=""
+tsub=[]
+tpub=[]
+msg_rep=""
 
-msg_lab_nam.place(x=10, y= 60, width=150, height = 20)
-msg_lab_top.place(x=10, y= 20, width=150, height = 20)
-msg_en_nam.place(x=200, y= 60, width=150, height = 20)
-msg_en_top.place(x=200, y= 20, width=150, height = 20)
-msg_button.place(x=162.5, y= 120, width=75, height = 30)
-root1.mainloop()
+name = input("Enter User Name: ")
+fk = open(name+".ps","a+")
+fk.seek(0,0)
+pws=input("Enter Pass.: ")
 
+# client creation
 client = mqtt.Client()
 client.on_connect = on_connect
-client.message_callback_add(topic_name, lis)
+client.message_callback_add("auth_ser@"+name, authrec)
+client.message_callback_add("serv@"+name, eventNews)
 client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
 client.username_pw_set("example", "Example123")
 client.connect("49d1a22e3af240efb207d443419e3257.s1.eu.hivemq.cloud", 8883)
 
-client.loop_start()
-client.subscribe(topic_name)
-
-msg_string=""
-
-root =Tk()
-root.geometry('304x500')
-root.title('lolChat>>> Topic: '+topic_name+' User: '+name)
-frame1=Frame(root, width=304, height=420, background="#ededed")
-frame1_5=Frame(root, width= 304, height=29,background="#ededed")
-frame2=Frame(root, width=304, height=50, background="#d4ffd4")
-
-
-frame1.pack(side=TOP, fill=X)
-msg_label=Label(frame1,text=msg_string, justify=LEFT,font=("Times Roman",12),anchor=W)
-msg_label.place(x=5, y=5,width=294, height = 300 )
-
-
-
-frame2.pack(side=BOTTOM, fill=X)
-msg_entry = Entry(frame2,justify=LEFT, font=("", 14))
-msg_button = Button(frame2, text=">>>",command=send)
-msg_entry.place(x=5, y= 5, width=240, height = 40)
-msg_button.place(x=254, y= 5, width=40, height = 40)
-
-frame1_5.pack(side=BOTTOM, fill=X)
-e_1 = Button(frame1_5, text="\U0001F606" ,font=("",12), command=lambda: add_emoji("\U0001F606"))
-e_1.place(x=2, y= 2, width=25, height = 25)
-e_2 = Button(frame1_5, text="\U0001F923" ,font=("",12), command=lambda: add_emoji("\U0001F923"))
-e_2.place(x=27, y= 2, width=25, height = 25)
-e_3 = Button(frame1_5, text="\U0001F643" ,font=("",12), command=lambda: add_emoji("\U0001F643"))
-e_3.place(x=52, y= 2, width=25, height = 25)
-e_4 = Button(frame1_5, text="\U0001F605" ,font=("",12), command=lambda: add_emoji("\U0001F605"))
-e_4.place(x=77, y= 2, width=25, height = 25)
-e_5 = Button(frame1_5, text="\U0001F607" ,font=("",12), command=lambda: add_emoji("\U0001F607"))
-e_5.place(x=102, y= 2, width=25, height = 25)
-e_6 = Button(frame1_5, text="\U0001F611" ,font=("",12), command=lambda: add_emoji("\U0001F611"))
-e_6.place(x=127, y= 2, width=25, height = 25)
-e_7 = Button(frame1_5, text="\U0001F60F" ,font=("",12), command=lambda: add_emoji("\U0001F60F"))
-e_7.place(x=152, y= 2, width=25, height = 25)
-e_8 = Button(frame1_5, text="\U0001F612" ,font=("",12), command=lambda: add_emoji("\U0001F612"))
-e_8.place(x=177, y= 2, width=25, height = 25)
-e_9 = Button(frame1_5, text="\U0001F44C" ,font=("",12), command=lambda: add_emoji("\U0001F44C"))
-e_9.place(x=202, y= 2, width=25, height = 25)
-e_10 = Button(frame1_5, text="\U0001F918" ,font=("",12), command=lambda: add_emoji("\U0001F918"))
-e_10.place(x=227, y= 2, width=25, height = 25)
-e_11 = Button(frame1_5, text="\U0001F44D" ,font=("",12), command=lambda: add_emoji("\U0001F44D"))
-e_11.place(x=252, y= 2, width=25, height = 25)
-e_12 = Button(frame1_5, text="\U0001F64F" ,font=("",12), command=lambda: add_emoji("\U0001F64F"))
-e_12.place(x=277, y= 2, width=25, height = 25)
-
-t1=threading.Thread(target=root.mainloop())
+if(fk.readlines() == []):
+    fk.write("name: "+name+'\n')//6
+    fk.write("pass: "+pws+'\n')
+else:
+    fk.seek(0,0)
+    data = fk.readlines()
+    pws_ret=data[1][6:-1]
+    if(pws!=pws_ret):
+        print("Wrong Password")
+        exit()
+    if(len(data)>2):
+        tsub.extend(data[2][6:-2].split(","))
+        tpub.extend(data[3][6:-2].split(","))
+#--- to enter topic ---
+t1 = threading.Thread(target=add_topic)
 t1.start()
-exit()
+if(len(tsub)==0):
+    print("No, topic present exiting......")
+    exit()
+#--- if no topic exit --
+for sb in tpub:
+    client.message_callback_add(sb, subFunc)
+
+client.subscribe("auth_ser@"+name)
+client.subscribe("serv@"+name)
+for sb in tpub:
+    client.subscribe(sb)
+pubFunc("mayank<@>Hello",tsub[0])
+client.loop_forever()
+# subscribe_all()
+
+# msg_string=""
+
+# root =Tk()
+# root.geometry('304x500')
+# root.title('lolChat>>> Topic: '+topic_name+' User: '+name)
+# frame1=Frame(root, width=304, height=420, background="#ededed")
+# frame1_5=Frame(root, width= 304, height=29,background="#ededed")
+# frame2=Frame(root, width=304, height=50, background="#d4ffd4")
+
+
+# frame1.pack(side=TOP, fill=X)
+# msg_label=Label(frame1,text=msg_string, justify=LEFT,font=("Times Roman",12),anchor=W)
+# msg_label.place(x=5, y=5,width=294, height = 300 )
+
+
+# frame2.pack(side=BOTTOM, fill=X)
+# msg_entry = Entry(frame2,justify=LEFT, font=("", 14))
+# msg_button = Button(frame2, text=">>>",command=send)
+# msg_entry.place(x=5, y= 5, width=240, height = 40)
+# msg_button.place(x=254, y= 5, width=40, height = 40)
+
+# frame1_5.pack(side=BOTTOM, fill=X)
+# e_1 = Button(frame1_5, text="\U0001F606" ,font=("",12), command=lambda: add_emoji("\U0001F606"))
+# e_1.place(x=2, y= 2, width=25, height = 25)
+# e_2 = Button(frame1_5, text="\U0001F923" ,font=("",12), command=lambda: add_emoji("\U0001F923"))
+# e_2.place(x=27, y= 2, width=25, height = 25)
+# e_3 = Button(frame1_5, text="\U0001F643" ,font=("",12), command=lambda: add_emoji("\U0001F643"))
+# e_3.place(x=52, y= 2, width=25, height = 25)
+# e_4 = Button(frame1_5, text="\U0001F605" ,font=("",12), command=lambda: add_emoji("\U0001F605"))
+# e_4.place(x=77, y= 2, width=25, height = 25)
+# e_5 = Button(frame1_5, text="\U0001F607" ,font=("",12), command=lambda: add_emoji("\U0001F607"))
+# e_5.place(x=102, y= 2, width=25, height = 25)
+# e_6 = Button(frame1_5, text="\U0001F611" ,font=("",12), command=lambda: add_emoji("\U0001F611"))
+# e_6.place(x=127, y= 2, width=25, height = 25)
+# e_7 = Button(frame1_5, text="\U0001F60F" ,font=("",12), command=lambda: add_emoji("\U0001F60F"))
+# e_7.place(x=152, y= 2, width=25, height = 25)
+# e_8 = Button(frame1_5, text="\U0001F612" ,font=("",12), command=lambda: add_emoji("\U0001F612"))
+# e_8.place(x=177, y= 2, width=25, height = 25)
+# e_9 = Button(frame1_5, text="\U0001F44C" ,font=("",12), command=lambda: add_emoji("\U0001F44C"))
+# e_9.place(x=202, y= 2, width=25, height = 25)
+# e_10 = Button(frame1_5, text="\U0001F918" ,font=("",12), command=lambda: add_emoji("\U0001F918"))
+# e_10.place(x=227, y= 2, width=25, height = 25)
+# e_11 = Button(frame1_5, text="\U0001F44D" ,font=("",12), command=lambda: add_emoji("\U0001F44D"))
+# e_11.place(x=252, y= 2, width=25, height = 25)
+# e_12 = Button(frame1_5, text="\U0001F64F" ,font=("",12), command=lambda: add_emoji("\U0001F64F"))
+# e_12.place(x=277, y= 2, width=25, height = 25)
+
+# t1=threading.Thread(target=root.mainloop())
+# t1.start()
+# fk.close()
+# exit()
